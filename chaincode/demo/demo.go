47026248a097ee442c656bd07ef7e457848dbfa4
Pipapa <yezhengmaolove@gmail.com>
2020-03-02 16:54:38 +0800

    [fix]添加 1.4.6 chiancode demo

--- chaincode/demo/demo.go
+++ chaincode/demo/demo.go
@@ -16,20 +16,26 @@ limitations under the License.
 
 package main
 
+//WARNING - this chaincode's ID is hard-coded in chaincode_example04 to illustrate one way of
+//calling chaincode from a chaincode. If this example is modified, chaincode_example04.go has
+//to be modified as well with the new ID of chaincode_example02.
+//chaincode_example05 show's how chaincode ID can be passed in as a parameter instead of
+//hard-coding.
+
 import (
 	"fmt"
 	"strconv"
 
-	"github.com/hyperledger/fabric-chaincode-go/shim"
-	pb "github.com/hyperledger/fabric-protos-go/peer"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
-// ABstore Chaincode implementation
-type ABstore struct {
+// SimpleChaincode example simple Chaincode implementation
+type SimpleChaincode struct {
 }
 
-func (t *ABstore) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	fmt.Println("ABstore Init")
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	fmt.Println("ex02 Init")
 	_, args := stub.GetFunctionAndParameters()
 	var A, B string    // Entities
 	var Aval, Bval int // Asset holdings
@@ -66,8 +72,8 @@ func (t *ABstore) Init(stub shim.ChaincodeStubInterface) pb.Response {
 	return shim.Success(nil)
 }
 
-func (t *ABstore) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-	fmt.Println("ABstore Invoke")
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	fmt.Println("ex02 Invoke")
 	function, args := stub.GetFunctionAndParameters()
 	if function == "invoke" {
 		// Make payment of X units from A to B
@@ -84,7 +90,7 @@ func (t *ABstore) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 }
 
 // Transaction makes payment of X units from A to B
-func (t *ABstore) invoke(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (t *SimpleChaincode) invoke(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	var A, B string    // Entities
 	var Aval, Bval int // Asset holdings
 	var X int          // Transaction value
@@ -141,7 +147,7 @@ func (t *ABstore) invoke(stub shim.ChaincodeStubInterface, args []string) pb.Res
 }
 
 // Deletes an entity from state
-func (t *ABstore) delete(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (t *SimpleChaincode) delete(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	if len(args) != 1 {
 		return shim.Error("Incorrect number of arguments. Expecting 1")
 	}
@@ -158,7 +164,7 @@ func (t *ABstore) delete(stub shim.ChaincodeStubInterface, args []string) pb.Res
 }
 
 // query callback representing the query of a chaincode
-func (t *ABstore) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (t *SimpleChaincode) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	var A string // Entities
 	var err error
 
@@ -186,8 +192,8 @@ func (t *ABstore) query(stub shim.ChaincodeStubInterface, args []string) pb.Resp
 }
 
 func main() {
-	err := shim.Start(new(ABstore))
+	err := shim.Start(new(SimpleChaincode))
 	if err != nil {
-		fmt.Printf("Error starting ABstore chaincode: %s", err)
+		fmt.Printf("Error starting Simple chaincode: %s", err)
 	}
 }
