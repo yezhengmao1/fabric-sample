4249126d2c4555af771475324cb5238ff66495f6
Pipapa <yezhengmaolove@gmail.com>
2020-03-03 17:35:04 +0800

    [feat]添加脚本,修改私钥文件名

--- solo-network/scripts/utils.sh
+++ solo-network/scripts/utils.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+
+GetFile() {
+    files=$(ls $1)
+    echo ${files[0]}
+    cd $1
+    cp ${files[0]} ./key.pem
+    cd -
+}
+
 case $1 in
+    pk)
+        GetFile crypto-config/peerOrganizations/orga.com/users/Admin@orga.com/msp/keystore
+        GetFile crypto-config/peerOrganizations/orga.com/users/User1@orga.com/msp/keystore
+        GetFile crypto-config/peerOrganizations/orgb.com/users/Admin@orgb.com/msp/keystore
+        GetFile crypto-config/peerOrganizations/orgb.com/users/User1@orgb.com/msp/keystore
+        ;;
     clean)
         rm -rf ./channel-artifacts
         rm -rf ./crypto-config
