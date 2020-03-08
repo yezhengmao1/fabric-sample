9c602c0386a7f5fe30bf589b93b697a42b91672d
Pipapa <yezhengmaolove@gmail.com>
2020-03-08 14:23:59 +0800

    [feat]添加多通道网络

--- README.md
+++ README.md
@@ -34,3 +34,34 @@ peer:
 	172.22.0.4/peer0.orgb.com:8051
 ```
 
+## multi-channel-network
+
+网络（* - 锚节点）
+
+```
+orderer:
+	172.22.0.2/orderer.example.com:7050
+peer:
+	orga:
+		172.22.0.3/peer0.orgA.example.com:7051(*)
+	orgb:
+		172.22.0.4/peer0.orgB.example.com:8051(*)
+		172.22.0.5/peer1.orgB.example.com:8061
+		172.22.0.6/peer2.orgB.example.com:8071
+		172.22.0.7/peer3.orgB.example.com:8081
+		172.22.0.8/peer4.orgB.example.com:8091
+	orgc:
+		172.22.0.9/peer0.orgC.example.com:9051(*)
+		172.22.0.10/peer1.orgC.example.com:9061
+		172.22.0.11/peer2.orgC.example.com:9071
+		172.22.0.12/peer3.orgC.example.com:9081
+		172.22.0.13/peer4.orgC.example.com:9091
+	orgd:
+		172.22.0.14/peer0.orgD.example.com:10051(*)
+		172.22.0.15/peer1.orgD.example.com:10061
+		172.22.0.16/peer2.orgD.example.com:10071
+		172.22.0.17/peer3.orgD.example.com:10081
+		172.22.0.18/peer4.orgD.example.com:10091
+		172.22.0.19/peer5.orgD.example.com:10101
+```
+
---
+++ multi-channel-network/scripts/gen.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+GENESIS_PROFILE=Genesis
+CHANNEL_PROFILE=(ChannelABCD ChannelBC ChannelBCD ChannelCD)
+CHANNEL_ID=(channelabcd channelbc channlebcd channelcd)
+CHANNEL_ANCHOR=('OrgAMSP OrgBMSP OrgCMSP OrgDMSP' 'OrgBMSP OrgCMSP' 'OrgBMSP OrgCMSP OrgDMSP' 'OrgCMSP OrgDMSP')
+
+SYS_CHANNEL=sys-channel
+VERSION=1.4.4
+
+FABRIC_CFG_PATH=$PWD
+
+# 检测cryptogen和版本
+if ! [ -x "$(command -v cryptogen)" ] ; then
+    echo -e "\033[31m no cryptogen\033[0m"
+    exit 1
+fi
+if [ ${VERSION} != "$(cryptogen version | grep Version | awk -F ': ' '{print $2}')" ] ; then
+    echo -e "\033[31m cryptogen need version \033[0m"${VERSION}
+    exit 1
+fi
+# 检测configtxgen和版本
+if ! [ -x "$(command -v configtxgen)" ] ; then 
+    echo -e "\033[31m no configtxgen\033[0m"
+    exit 1
+fi
+if [ ${VERSION} != "$(configtxgen --version | grep Version | awk -F ': ' '{print $2}')" ] ; then
+    echo -e "\033[31m configtxgen need version \033[0m"${VERSION}
+    exit 1
+fi
+# 生成证书文件
+echo -e "\033[31m clear crypto files\033[0m"
+rm -rf crypto-config
+echo -e "\033[31m generate crypto files\033[0m"
+cryptogen generate --config ./crypto-config.yaml
+# 清理多余文件
+echo -e "\033[31m clear block files\033[0m"
+rm -rf ./channel-artifacts
+mkdir ./channel-artifacts
+# 生成创世块
+echo -e "\033[31m generate genesis block\033[0m"
+configtxgen \
+    -profile ${GENESIS_PROFILE} \
+    -channelID ${SYS_CHANNEL} \
+    -outputBlock ./channel-artifacts/genesis.block \
+# 生成通道交易
+echo -e "\033[31m generate channel transcation\033[0m"
+for i in ${CHANNEL_PROFILE[@]}; do
+    configtxgen \
+        -profile ${i} \
+        -channelID ${i} \
+        -outputCreateChannelTx ./channel-artifacts/${i}.tx
+done
+# 生成铆节点配置
+for chi in ${!CHANNEL_ANCHOR[@]}; do
+    echo -e "\033[31m generate anchor transcation for \033[0m" ${CHANNEL_ID[$chi]}
+    ORGS=${CHANNEL_ANCHOR[$chi]}
+    for i in ${ORGS[@]}; do
+        configtxgen \
+            -profile ${CHANNEL_PROFILE[$chi]}\
+            -channelID ${CHANNEL_ID[$chi]} \
+            -outputAnchorPeersUpdate ./channel-artifacts/${i}anchor_${CHANNEL_ID[$chi]}.tx \
+            -asOrg ${i}
+        echo $i
+    done
+done
